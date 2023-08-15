f050bc09fce8fd53c929ae7ddf4d10a4da63c3b6
amira-creator <amira.abdelfattah99@gmail.com>
2023-08-15 10:03:34 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""define ajson file writting function."""
-import json
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-def save_to_json_file(my_obj, filename)
-   """write an object with atext file usig json representation"""
-   with open(filename, "w") as f:
-       json.dump(my_obj, f)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
