03fda4fd7a980b95b70a845f40bd8bdd102416f0
amira-creator <amira.abdelfattah99@gmail.com>
2023-11-03 08:23:23 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
-if __name__ == "__main__":
-    fetch_url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(fetch_url) as response:
-        output = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(output)))
-        print("\t- content: {}".format(output))
-        print("\t- utf8 content: {}".format(output.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
