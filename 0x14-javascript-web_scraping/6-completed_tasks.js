2ce80c41a0a532532a2df84910739b4ebcf0cd83
amira-creator <amira.abdelfattah99@gmail.com>
2023-11-28 04:29:44 -0800

    add file

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,63 +1,33 @@
 #!/usr/bin/node
 
-const URL = process.argv[2];
 const request = require('request');
 
-request.get(URL, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const apiUrl = process.argv[2];
 
-  if (response.statusCode !== 200) {
-    console.log(error);
-    return;
-  }
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    try {
+      const todos = JSON.parse(body);
 
-  const todos = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!todos[todo.userId]) {
-        todos[todo.userId] = 1;
-      } else {
-        todos[todo.userId]++;
-      }
-    }
-  });
+      const completed = {};
 
-  const length = Object.keys(todos).length;
-  if (length === 0) {
-    console.log('{}');
-    return;
-  }
-
-  if (length === 2) {
-    let output = '{ ';
-    let isFirst = true;
-    for (const key in todos) {
-      if (!isFirst) {
-        output += ', ';
-      }
-      output += `'${key}': ${todos[key]}`;
-      isFirst = false;
-    }
-    output += ' }';
-    console.log(output);
-  } else {
-    let task = 0;
-    for (const key in todos) {
-      if (task === 0) {
-        if (length !== 1) {
-          console.log('{ \'' + key + '\': ' + todos[key] + ',');
-        } else {
-          console.log('{ \'' + key + '\': ' + todos[key] + ' }');
+      todos.forEach((todo) => {
+        if (todo.completed) {
+          if (completed[todo.userId] === undefined) {
+            completed[todo.userId] = 1;
+          } else {
+            completed[todo.userId]++;
+          }
         }
-      } else if (task === length - 1) {
-        console.log('  \'' + key + '\': ' + todos[key] + ' }');
-      } else {
-        console.log(`  '${key}': ${todos[key]},`);
-      }
-      task++;
+      });
+
+      const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
+
+      console.log(Object.keys(completed).length > 2 ? output : completed);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
     }
+  } else {
+    console.error('Error:', error);
   }
 });
